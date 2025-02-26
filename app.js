cc9a7024a219abeca1171cca9c5286f2f2ebe3cb
Филипп <filippalekseev@MacBook-Air-Filipp.local>
2025-02-26 15:22:37 +0300

    22

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Main route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// 404 Handling
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
